598899bbf4b81f7271fe77bd6268f710a71c71c1
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-04-30 10:11:20 +0200

    Change name that user logged in app & continue

--- 08-journal-app/src/components/journal/Sidebar.js
+++ 08-journal-app/src/components/journal/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { JournalEntries } from './JournalEntries';
 import { startLogout } from '../../actions/auth';
@@ -7,6 +7,8 @@ import { startLogout } from '../../actions/auth';
 export const Sidebar = () => {
 
     const dispatch = useDispatch();
+    // Get user logged in the app
+    const { name } = useSelector( state => state.auth );
 
     const handleLogout = () => {
         dispatch( startLogout() );
@@ -18,7 +20,7 @@ export const Sidebar = () => {
             <div className="journal__sidebar-navbar">
                 <h3 className="mt-5">
                     <i className="far fa-moon"></i>
-                    <span> Jesus</span>
+                    <span> { name } </span>
                 </h3>
 
                 <button 
--- 08-journal-app/src/routers/AppRouter.js
+++ 08-journal-app/src/routers/AppRouter.js
@@ -41,7 +41,7 @@ export const AppRouter = () => {
 
     if ( checking ) {
         return (
-            <h1>Espere...</h1>
+            <h1>Wait...</h1>
         )
     }
 

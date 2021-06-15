1c473a03a42afdcfefec7b0aca527b0e0283c73a
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-06-15 12:16:04 +0200

    Continue with Tests - Working in auth.test.js :wrench:

--- 09-calendar-app/src/tests/helpers/fetch.test.js
+++ 09-calendar-app/src/tests/helpers/fetch.test.js
@@ -1,7 +1,9 @@
-import { fetchWitoutToken } from "../../helpers/fetch"
+import { fetchWithToken, fetchWitoutToken } from "../../helpers/fetch";
 
 describe('Test in helper Fetch', () => {
     
+    let token = '';
+
     test('fetchWitOutToken should run', async () => {
 
         const resp = await fetchWitoutToken('auth', { email: 'hola@jesus.com', password: '123456789'}, 'POST');
@@ -9,7 +11,20 @@ describe('Test in helper Fetch', () => {
         expect( resp instanceof Response ).toBe(true);
 
         const body = await resp.json();
-        expect( body ).toBe(true);
+        expect( body.ok ).toBe(true);
+
+        token = body.token;
+
+    })
+
+    test('fetchWitOutToken should run', async () => {
+
+        localStorage.setItem('token', token);
+
+        const resp = await fetchWithToken('events/60c3285248248e3ec494382ab', {}, 'DELETE');
+        const body = await resp.json();
+
+        expect( body.msg ).toBe('Speak with the Administrator, please!');
 
     })
     
